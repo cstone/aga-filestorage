6ee3682338f8afbd049ca928b58269b9905d11c3
Cavin Stone <cstone2003@hotmail.com>
2015-03-24 08:29:14 -0400

    Update carrierwave initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,6 @@ CarrierWave.configure do |config|
   config.storage    = :aws
   config.aws_bucket = ENV.fetch('AWS_S3_BUCKET')
   config.aws_acl    = :public_read
-  config.asset_host = 'http://example.com'
-  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
 
   config.aws_credentials = {
       access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
